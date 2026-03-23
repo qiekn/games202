13fae069f9d5adb532378e8253b5e1658a40100a
qiekn <qiekndev@gmail.com>
2026-03-23 22:21:17 +0800

    pa1: read loadShader.js

--- homework/hw1/src/loads/loadShader.js
+++ homework/hw1/src/loads/loadShader.js
@@ -1,7 +1,8 @@
 async function loadShaderFile(filename) {
-  return new Promise((resolve, reject) => {
-    const loader = new THREE.FileLoader();
+  return new Promise((resolve) => {
+    const loader = new THREE.FileLoader(); // 调用 three.js 的 FileLoader()
 
+    // HTTP 请求 → 读取文件内容 → callback(data)
     loader.load(filename, (data) => {
       resolve(data);
       //console.log(data);
@@ -9,6 +10,7 @@ async function loadShaderFile(filename) {
   });
 }
 
+// 异步加载 shader 文件 (.glsl), 并返回字符串
 async function getShaderString(filename) {
   let val = "";
   await this.loadShaderFile(filename).then((result) => {
