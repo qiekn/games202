1dc73af69a9c9da53bf708b1ea5c1dd1ffcbc5b9
qiekn <qiekndev@gmail.com>
2026-04-12 11:35:45 +0800

    pa5: format → all .h & .cpp files with google code style

--- homework/hw5/src/util/common.h
+++ homework/hw5/src/util/common.h
@@ -4,14 +4,12 @@
 
 #include <iostream>
 
-#define LOG(msg)                                                                         \
-    std::cout << "[" << __FILE__ << ", " << __FUNCTION__ << ", " << __LINE__             \
-              << "]: " << msg << std::endl;
+#define LOG(msg) std::cout << "[" << __FILE__ << ", " << __FUNCTION__ << ", " << __LINE__ << "]: " << msg << std::endl;
 
-#define CHECK(cond)                                                                      \
-    do {                                                                                 \
-        if (!(cond)) {                                                                   \
-            LOG("Runtime Error.");                                                       \
-            exit(-1);                                                                    \
-        }                                                                                \
-    } while (false)
+#define CHECK(cond)          \
+  do {                       \
+    if (!(cond)) {           \
+      LOG("Runtime Error."); \
+      exit(-1);              \
+    }                        \
+  } while (false)
--- homework/hw5/src/util/imageutil.h
+++ homework/hw5/src/util/imageutil.h
@@ -2,11 +2,8 @@
 
 #include <string>
 
-float *ReadImage(const std::string &filename, int &width, int &height,
-                 const int &channel);
+float* ReadImage(const std::string& filename, int& width, int& height, const int& channel);
 
-float *ReadImageLayer(const std::string &filename, const std::string &layername,
-                      int &width, int &height, const int &channel);
+float* ReadImageLayer(const std::string& filename, const std::string& layername, int& width, int& height, const int& channel);
 
-bool WriteImage(const std::string &filename, const int &width, const int &height,
-                const int &channel, const float *buffer);
+bool WriteImage(const std::string& filename, const int& width, const int& height, const int& channel, const float* buffer);
